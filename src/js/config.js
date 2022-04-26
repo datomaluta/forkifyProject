b0c0723266f26cea30bb7348bbd7f4e145e8d19e
datomaluta <datomaluta@gmail.com>
2022-04-26 14:42:38 +0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4c1f1422-fd7a-4a96-a4bd-15140f6baffd';
+export const MODAL_CLOSE_SEC = 2.5;
